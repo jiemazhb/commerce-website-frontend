db35b79653d4c5f5ad9ae826b0ad649fb2343e1f
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-10-31 15:42:29 -0700

    clear all the files in branch

--- src/component/productPage/viewOrder.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ViewOrder() {
-  return (
-    <>
-           <button type="button" className="btn btn-outline-success mt-5 btn-sm">Order Details</button>
-    </>
-  )
-}
